c039225c9c45d55dd7ea60af531865be60740616
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-22 12:54:37 -0500

    This test validates the generateOneTimeStylesheet method of the DynamicCSSManager service

---
+++ Tests/Services/DynamicCSSManagerGenerateOneTimeTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: caldwecr
+ * Date: 10/22/13
+ * Time: 12:11 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Tests\Services;
+
+use Cympel\Bundle\AnalyticsBundle\Tests\ContainerAwareUnitTestCase;
+
+class DynamicCSSManagerGenerateOneTimeTest extends ContainerAwareUnitTestCase
+{
+    public function testDynamicCSSManagerGenerateOneTime()
+    {
+        $dcm = $this->get('cympel_analytics.dynamic_css_manager');
+        $ids = array(
+            'foo',
+            'bar'
+        );
+        $pseudo = 'hover';
+        $uri = $dcm->generateOneTimeStylesheet($ids, $pseudo);
+
+        $this->assertTrue(strpos($uri, '/analytics/dynamic/css/') !== false);
+    }
+}
