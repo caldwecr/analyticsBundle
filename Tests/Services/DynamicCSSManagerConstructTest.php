c3e34545ea7e31450c3da0bc6a268260c0c0b9b9
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-22 12:04:23 -0500

    Created a basic configuration validation test of the DynamicCSSManager service

---
+++ Tests/Services/DynamicCSSManagerConstructTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: caldwecr
+ * Date: 10/22/13
+ * Time: 11:58 AM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Tests\Services;
+
+use Cympel\Bundle\AnalyticsBundle\Tests\ContainerAwareUnitTestCase;
+
+class DynamicCSSManagerConstructTest extends ContainerAwareUnitTestCase
+{
+    public function testDynamicCSSManagerConstruct()
+    {
+        $dcm = $this->get('cympel_analytics.dynamic_css_manager');
+        $this->assertEquals('DynamicCSSManager', $dcm->getType());
+    }
+}
