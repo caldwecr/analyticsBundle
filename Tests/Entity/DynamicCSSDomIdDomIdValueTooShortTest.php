e996dcf23dcb56b32fa0873321282ddaf357bad2
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-22 15:37:01 -0500

    This test verifies that validation of the domIdValue property is working as expected for property values that are too short

---
+++ Tests/Entity/DynamicCSSDomIdDomIdValueTooShortTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: caldwecr
+ * Date: 10/22/13
+ * Time: 3:30 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Tests\Entity;
+
+use Cympel\Bundle\AnalyticsBundle\Tests\ContainerAwareUnitTestCase;
+use Cympel\Bundle\AnalyticsBundle\Tests\Entity\DynamicCSSDomIdTestInstanceFactory;
+
+class DynamicCSSDomIdDomIdValueTooShortTest extends ContainerAwareUnitTestCase
+{
+    public function testTooShortDomIdValue()
+    {
+        $domId = DynamicCSSDomIdTestInstanceFactory::createInstance();
+        $dcdim = $this->get('cympel_analytics.dynamic_css_dom_id_manager');
+        $domId->setDomIdValue('a');
+        $errors = $dcdim->validate($domId);
+        $this->assertEquals(1, count($errors));
+    }
+}
