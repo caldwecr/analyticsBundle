17a5bd9d8c4eb0ab8b8b5693990dcbf23d80da56
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-23 15:11:17 -0500

    Added a container aware unit test case for the DynamicCSSManager::create method

---
+++ Tests/Services/DynamicCSSManagerCreateTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/23/13
+ * Time: 2:36 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Tests\Services;
+
+use Cympel\Bundle\AnalyticsBundle\Entity\Tracker;
+use Cympel\Bundle\AnalyticsBundle\Tests\ContainerAwareUnitTestCase;
+
+class DynamicCSSManagerCreateTest extends ContainerAwareUnitTestCase
+{
+    public function testCreate()
+    {
+        $t = new Tracker();
+        // Validate that there aren't any tools associated with the tracker
+        $this->assertEquals(0, count($t->getTrackingTools()));
+
+        $dcm = $this->get('cympel_analytics.dynamic_css_manager');
+        $dcss = $dcm->create($t);
+        $this->assertTrue($dcss->getTracker()->equals($t));
+
+        // Validate that there is one tool associated with the tracker and that it is $dcss
+        $this->assertEquals(1, count($t->getTrackingTools()));
+        $tools = $t->getTrackingTools();
+        $this->assertTrue($tools[0]->equals($dcss));
+    }
+}
