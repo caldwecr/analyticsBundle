00407fe28dc4ce3a7cf83dc1e86ea3f8c0510044
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-23 14:04:47 -0500

    Created the initial version of the iTrackingTool interface now to apply it to the existing tools

---
+++ Entity/iTrackingTool.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/23/13
+ * Time: 1:43 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity;
+
+interface iTrackingTool extends iType
+{
+}
