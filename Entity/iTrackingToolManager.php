d0ca1db5e76df04c78d00c40e95ac01c4bd4d8e0
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-23 14:04:10 -0500

    Created initial version of the interface iTrackingToolManager, now to apply it to the existing tracking tool managers

---
+++ Entity/iTrackingToolManager.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/23/13
+ * Time: 1:46 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity;
+
+interface iTrackingToolManager extends iType
+{
+    /**
+     * @param iTracker $tracker
+     * @return iTrackingTool
+     *
+     * This method creates a brand new tracking tool that is a child to the first argument
+     */
+    public function create(iTracker $tracker);
+
+    /**
+     * @param iTrackingTool $tool
+     * @throws \Cympel\Bundle\AnalyticsBundle\Entity\Exception\InvalidTrackingToolException
+     * @return bool
+     *
+     * This method should persist a tracking tool to the database
+     */
+    public function persist(iTrackingTool $tool);
+
+    /**
+     * @param iTrackingTool $tool
+     * @throws \Cympel\Bundle\AnalyticsBundle\Entity\Exception\InvalidTrackingToolException
+     * @return bool
+     *
+     * This method should remove a tracking tool from the database
+     */
+    public function remove(iTrackingTool $tool);
+
+    /**
+     * @param $id
+     * @return iTrackingTool
+     *
+     * This method should scan the database for an instance of the TrackingTool of appropriate type and id
+     */
+    public function findOneById($id);
+
+    /**
+     * @param $entityManagerName
+     * @return void
+     *
+     * This method must set the manager's entity manager name property
+     */
+    public function setEntityManagerName($entityManagerName);
+
+    /**
+     * @return string
+     *
+     * This method must return the manager's entity manager name
+     */
+    public function getEntityManagerName();
+
+}
