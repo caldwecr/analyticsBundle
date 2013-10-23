562bf10c00b8a958aac6b05bff4acd26bff5616c
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-23 15:10:32 -0500

    Added a TrackerManager service and configured it

---
+++ Services/TrackerManager.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/23/13
+ * Time: 2:41 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services;
+
+use Cympel\Bundle\AnalyticsBundle\Entity\iTrackingTool;
+use Cympel\Bundle\AnalyticsBundle\Entity\iTracker;
+use Cympel\Bundle\AnalyticsBundle\Entity\iType;
+
+class TrackerManager implements iType
+{
+
+    /**
+     * @param iTracker $tracker
+     * @param iTrackingTool $tool
+     * @return bool
+     */
+    public function addTrackingTool(iTracker $tracker, iTrackingTool $tool)
+    {
+        $tools = $tracker->getTrackingTools();
+        var_dump($tools);
+        $tools->add($tool);
+        $tracker->setTrackingTools($tools);
+        return true;
+    }
+    /**
+     * @return string
+     * This method must return a string with a unique representation of the object type that is implementing this interface
+     */
+    public function getType()
+    {
+        return 'TrackerManager';
+    }
+
+}
