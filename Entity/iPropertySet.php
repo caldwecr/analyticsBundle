8ab2d345da58cd90bbe2a979ac0bf586ceb32c06
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-23 16:32:16 -0500

    Created new base interface for typed property sets

---
+++ Entity/iPropertySet.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/23/13
+ * Time: 4:31 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity;
+
+interface iPropertySet extends iType
+{
+
+}
