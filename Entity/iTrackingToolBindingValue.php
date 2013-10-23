bbdbe07ce9df27c4949840f1e9e42915775fea65
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-23 16:16:03 -0500

    Added two base interfaces for the TrackingToolBinding

---
+++ Entity/iTrackingToolBindingValue.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/23/13
+ * Time: 4:14 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity;
+
+interface iTrackingToolBindingValue extends iType
+{
+
+}
