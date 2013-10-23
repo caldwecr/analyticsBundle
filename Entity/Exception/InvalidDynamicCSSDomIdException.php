10d2a7eadb688893fbcd3e9b8af3c684d4e06622
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-23 15:13:11 -0500

    Updated the namespace

---
+++ Entity/Exception/InvalidDynamicCSSDomIdException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: caldwecr
+ * Date: 10/22/13
+ * Time: 7:27 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity\Exception;
+
+class InvalidDynamicCSSDomIdException extends \Exception
+{
+
+}
