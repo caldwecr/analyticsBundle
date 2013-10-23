0cc5ba3797494e8371750480aefe152c3f969e81
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-22 20:14:49 -0500

    Created test method for the renderByDynamicCSSAndDomIdValue

---
+++ Tests/Services/DynamicCSSDomIdManagerRenderByDCSSAndDomIdValueTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: caldwecr
+ * Date: 10/22/13
+ * Time: 8:07 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Tests\Services;
+
+use Cympel\Bundle\AnalyticsBundle\Tests\ContainerAwareUnitTestCase;
+use Cympel\Bundle\AnalyticsBundle\Tests\Entity\DynamicCSSDomIdTestInstanceFactory;
+
+class DynamicCSSDomIdManagerRenderByDCSSAndDomIdValueTest extends ContainerAwareUnitTestCase
+{
+    public function testRenderByDCSSAndDomIdValue()
+    {
+        $dcdim = $this->get('cympel_analytics.dynamic_css_dom_id_manager');
+        $dcssdi = DynamicCSSDomIdTestInstanceFactory::createInstance('DynamicCSSDomIdManagerRenderByDCSSAndDomIdValueTest');
+        $dcdim->persist($dcssdi);
+        $dcssdi2 = $dcdim->findOneByDynamicCSSAndDomIdValue($dcssdi->getDynamicCSS(), $dcssdi->getDomIdValue());
+        $this->assertTrue($dcssdi->equals($dcssdi2));
+        $this->assertEquals(0, $dcssdi2->getRendered());
+
+        // This section validates that the rendered property has been set and persisted
+        $dcssdi3 = $dcdim->renderByDynamicCSSAndDomIdValue($dcssdi->getDynamicCSS(), $dcssdi->getDomIdValue());
+        $this->assertNotEquals(0, $dcssdi3->getRendered());
+        $this->assertTrue($dcssdi->equals($dcssdi3));
+        $this->assertNotEquals(0, $dcssdi2->getRendered());
+    }
+}
