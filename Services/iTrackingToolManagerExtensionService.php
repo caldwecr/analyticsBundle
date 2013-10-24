e9339d168d9478c2e5f75aa44479b73e863138a0
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-24 13:15:03 -0500

    Added a new service interface

---
+++ Services/iTrackingToolManagerExtensionService.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/24/13
+ * Time: 1:14 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services;
+
+use Cympel\Bundle\AnalyticsBundle\Entity\iType;
+
+interface iTrackingToolManagerExtensionService extends iType
+{
+
+}
