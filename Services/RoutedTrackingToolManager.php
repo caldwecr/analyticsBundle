dcd77ab78fc4264bae956561fc7820bfb1588568
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-24 12:49:53 -0500

    Added intermediate abstract class on top of the TrackingToolManager that includes access to router functionality

---
+++ Services/RoutedTrackingToolManager.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/24/13
+ * Time: 12:48 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services;
+
+abstract class RoutedTrackingToolManager extends TrackingToolManager
+{
+    /**
+     * @param $doctrine
+     * @param $validator
+     * @param $router
+     * @param TrackerManager $trackerManager
+     * @param string $entityManagerName
+     */
+    abstract public function __construct($doctrine, $validator, $router, TrackerManager $trackerManager, $entityManagerName);
+}
