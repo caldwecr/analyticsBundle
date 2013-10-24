e7c2e1f745ccf5fa3f95846f20d3515e09719426
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-24 13:27:26 -0500

    Added new service class exception

---
+++ Services/Exception/InvalidTrackingToolManagerExtensionService.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/24/13
+ * Time: 1:26 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services\Exception;
+
+class InvalidTrackingToolManagerExtensionService extends \Exception
+{
+
+}
