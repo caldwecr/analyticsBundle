f2f80985808ef4d2c40d3160d255671781abf3a9
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-24 14:45:56 -0500

    Added class DynamicCSSDomIdArrayCollection

---
+++ Entity/DynamicCSSDomIdArrayCollection.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/24/13
+ * Time: 2:45 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+class DynamicCSSDomIdArrayCollection extends ArrayCollection
+{
+
+}
