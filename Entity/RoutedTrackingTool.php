5eb3f1acc4463702b3c820c8b57fa3bf69ac865c
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-25 09:46:27 -0500

    Added another intermediate abstract class

---
+++ Entity/RoutedTrackingTool.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/25/13
+ * Time: 9:45 AM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity;
+
+abstract class RoutedTrackingTool extends RenderableTrackingTool
+{
+
+}
