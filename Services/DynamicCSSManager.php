e03258af2facadf937b2db8a92ecf47c34f6a2b2
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-25 09:29:47 -0500

    Reorganized

--- Services/DynamicCSSManager.php
+++ Services/DynamicCSSManager.php
@@ -17,15 +17,10 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 class DynamicCSSManager extends RoutedTrackingToolManager
 {
     protected $doctrine;
-
     protected $router;
-
     protected $validator;
-
     protected $trackerManager;
-
     protected $emName;
-
     /**
      * @var DynamicCSSServiceExtension
      */
@@ -70,6 +65,108 @@ class DynamicCSSManager extends RoutedTrackingToolManager
     }
 
     /**
+     * @return TrackerManager
+     */
+    protected function getTrackerManager()
+    {
+        return $this->trackerManager;
+    }
+
+    /**
+     * @param TrackerManager $trackerManager
+     * @return void
+     */
+    protected function setTrackerManager(TrackerManager $trackerManager)
+    {
+        $this->trackerManager = $trackerManager;
+    }
+
+    /**
+     * @param DynamicCSS $dynamicCSS
+     * @return array
+     *
+     * This method is included so that the url property of the DynamicCSSDomId class can be populated even though this field is not persisted to the database
+     *  and thus accessed via the twig template at render time.
+     */
+    public function getDynamicCSSDomIds(DynamicCSS $dynamicCSS)
+    {
+        $ids = $dynamicCSS->getDynamicCSSDomIds()->toArray();
+        foreach ($ids as $key => $value) {
+            $value->setUrl(
+                $this->router->generate('dynamicCSSImageFile',
+                    array(
+                        'key' => $dynamicCSS->getId(),
+                        'domIdValue' => $value->getDomIdValue(),
+                    ),
+                    URLGeneratorInterface::ABSOLUTE_PATH
+                )
+            );
+        }
+        return $ids;
+    }
+
+    /**
+     * @return string
+     * This method must return a string with a unique representation of the object type that is implementing this interface
+     */
+    public function getType()
+    {
+        return 'DynamicCSSManager';
+    }
+
+    /**
+     * @param DynamicCSS $dynamicCSS
+     */
+    public function removeOneTimeStylesheet(DynamicCSS $dynamicCSS)
+    {
+        $em = $this->doctrine->getManager($this->emName);
+        $em->remove($dynamicCSS);
+        $em->flush();
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     *
+     * This method is invoked by the Default Controller to render a DynamicCSS
+     */
+    public function renderDCSSById($id)
+    {
+        $toReturn = $this->findOneTimeStylesheetById($id);
+        $toReturn->setRendered(time());
+        $em = $this->doctrine->getManager($this->emName);
+        $em->persist($toReturn);
+        $em->flush();
+        return $toReturn;
+    }
+
+    /**
+     * @param $id
+     * @return DynamicCSS
+     *
+     */
+    public function findOneTimeStylesheetById($id)
+    {
+        return $this->findOneById($id);
+    }
+
+    /**
+     * @return iPropertySet
+     */
+    protected function createPropertySet()
+    {
+        return new DynamicCSSPropertySet();
+    }
+
+    /**
+     * @return iTrackingTool
+     */
+    protected function createTrackingTool()
+    {
+        return new DynamicCSS();
+    }
+
+    /**
      * @param iPropertySet $properties
      * @param iTrackingTool $tool
      * @return \Cympel\Bundle\AnalyticsBundle\Entity\DynamicCSSDomIdArrayCollection|iPropertySet
@@ -96,155 +193,6 @@ class DynamicCSSManager extends RoutedTrackingToolManager
         $dynamicCSSDomIdArrayCollection = $properties->getIds();
         $this->dynamicCSSServiceExtension->getDynamicCSSDomIdArrayCollectionManager()->attachToolToDynamicCSSDomIds($dynamicCSSDomIdArrayCollection, $tool);
         return $properties;
-    }
-
-    /**
-     * @return string
-     */
-    protected function getRouteName()
-    {
-        return 'dynamicCSS';
-    }
-
-    /**
-     * @param iTrackingTool $tool
-     * @return array
-     */
-    protected function getRoutingArray(iTrackingTool $tool)
-    {
-        return array(
-            'key' => $tool->getId(),
-        );
-    }
-
-    /**
-     * @return Object -- the router service
-     */
-    protected function getRouter()
-    {
-        return $this->router;
-    }
-
-    /**
-     * @param $router
-     * @return void
-     */
-    protected function setRouter($router)
-    {
-        $this->router = $router;
-    }
-
-
-    /**
-     * @param DynamicCSS $dynamicCSS
-     */
-    public function removeOneTimeStylesheet(DynamicCSS $dynamicCSS)
-    {
-        $em = $this->doctrine->getManager($this->emName);
-        $em->remove($dynamicCSS);
-        $em->flush();
-    }
-
-    /**
-     * @param DynamicCSS $dynamicCSS
-     * @return array
-     *
-     * This method is included so that the url property of the DynamicCSSDomId class can be populated even though this field is not persisted to the database
-     *  and thus accessed via the twig template at render time.
-     */
-    public function getDynamicCSSDomIds(DynamicCSS $dynamicCSS)
-    {
-        $ids = $dynamicCSS->getDynamicCSSDomIds()->toArray();
-        foreach($ids as $key => $value) {
-            $value->setUrl(
-                $this->router->generate('dynamicCSSImageFile',
-                    array(
-                        'key' => $dynamicCSS->getId(),
-                        'domIdValue' => $value->getDomIdValue(),
-                    ),
-                    URLGeneratorInterface::ABSOLUTE_PATH
-                )
-            );
-        }
-        return $ids;
-    }
-
-
-    /**
-     * @param $id
-     * @return DynamicCSS
-     *
-     */
-    public function findOneTimeStylesheetById($id)
-    {
-        return $this->findOneById($id);
-    }
-
-    /**
-     * @return string
-     */
-    protected function getRepositoryName()
-    {
-        return 'CympelAnalyticsBundle:DynamicCSS';
-    }
-
-    /**
-     * @return iPropertySet
-     */
-    protected function createPropertySet()
-    {
-        return new DynamicCSSPropertySet();
-    }
-
-
-    /**
-     * @param $id
-     * @return mixed
-     *
-     * This method is invoked by the Default Controller to render a DynamicCSS
-     */
-    public function renderDCSSById($id)
-    {
-        $toReturn = $this->findOneTimeStylesheetById($id);
-        $toReturn->setRendered(time());
-        $em = $this->doctrine->getManager($this->emName);
-        $em->persist($toReturn);
-        $em->flush();
-        return $toReturn;
-    }
-
-    /**
-     * @return string
-     * This method must return a string with a unique representation of the object type that is implementing this interface
-     */
-    public function getType()
-    {
-        return 'DynamicCSSManager';
-    }
-
-    /**
-     * @return iTrackingTool
-     */
-    protected function createTrackingTool()
-    {
-        return new DynamicCSS();
-    }
-
-    /**
-     * @return TrackerManager
-     */
-    protected function getTrackerManager()
-    {
-        return $this->trackerManager;
-    }
-
-    /**
-     * @param TrackerManager $trackerManager
-     * @return void
-     */
-    protected function setTrackerManager(TrackerManager $trackerManager)
-    {
-        $this->trackerManager = $trackerManager;
     }
 
     /**
@@ -279,6 +227,50 @@ class DynamicCSSManager extends RoutedTrackingToolManager
     protected function setEmName($emName)
     {
         $this->emName = $emName;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getRepositoryName()
+    {
+        return 'CympelAnalyticsBundle:DynamicCSS';
+    }
+
+    /**
+     * @return string
+     */
+    protected function getRouteName()
+    {
+        return 'dynamicCSS';
+    }
+
+    /**
+     * @return Object -- the router service
+     */
+    protected function getRouter()
+    {
+        return $this->router;
+    }
+
+    /**
+     * @param $router
+     * @return void
+     */
+    protected function setRouter($router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * @param iTrackingTool $tool
+     * @return array
+     */
+    protected function getRoutingArray(iTrackingTool $tool)
+    {
+        return array(
+            'key' => $tool->getId(),
+        );
     }
 
     /**
