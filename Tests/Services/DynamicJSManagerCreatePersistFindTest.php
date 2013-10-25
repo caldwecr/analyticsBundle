709703a9220686c12cdd32f8a158bc6c9de89b24
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-25 10:11:40 -0500

    Create test that verifies the create, persist, and findOneById functionality of the DynamicJSManager

---
+++ Tests/Services/DynamicJSManagerCreatePersistFindTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/25/13
+ * Time: 10:03 AM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Tests\Services;
+
+use Cympel\Bundle\AnalyticsBundle\Tests\ContainerAwareUnitTestCase;
+
+class DynamicJSManagerCreatePersistFindTest extends ContainerAwareUnitTestCase
+{
+    public function testCreatePersistFind()
+    {
+        $djm = $this->get('cympel_analytics.dynamic_js_manager');
+        $dj = $djm->create();
+        $djm->persist($dj);
+        $dj2 = $djm->findOneById($dj->getId());
+        $this->assertTrue($dj->equals($dj2));
+        $this->assertTrue($dj2->equals($dj));
+    }
+}
