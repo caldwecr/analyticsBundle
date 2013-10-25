d767b360acdecaa01d22a4b619931ebb4ab9dd3c
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-25 10:37:59 -0500

    Added test of TrackerManager create, persist, and findOneById methods

---
+++ Tests/Services/TrackerManagerCreatePersistFindTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/25/13
+ * Time: 10:23 AM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Tests\Services;
+
+use Cympel\Bundle\AnalyticsBundle\Tests\ContainerAwareUnitTestCase;
+
+class TrackerManagerCreatePersistFindTest extends ContainerAwareUnitTestCase
+{
+    public function testCreatePersistFind()
+    {
+        $trackerManager = $this->get('cympel_analytics.tracker_manager');
+        $tracker = $trackerManager->create();
+        $trackerManager->persist($tracker);
+        $tracker2 = $trackerManager->findOneById($tracker->getId());
+        $this->assertTrue($tracker->equals($tracker2));
+        $this->assertTrue($tracker2->equals($tracker));
+    }
+}
