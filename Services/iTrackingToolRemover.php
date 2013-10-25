dc2bc8630e6c0b2653efdba8aab94cb696384be1
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-25 11:48:11 -0500

    Added interface iTrackingToolRemover

---
+++ Services/iTrackingToolRemover.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/25/13
+ * Time: 11:46 AM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services;
+
+use Cympel\Bundle\AnalyticsBundle\Entity\iType;
+use Cympel\Bundle\AnalyticsBundle\Entity\iTracker;
+use Cympel\Bundle\AnalyticsBundle\Entity\iTrackingTool;
+
+interface iTrackingToolRemover extends iType
+{
+    /**
+     * @param iTracker $tracker
+     */
+    public function removeToolsFromTracker(iTracker $tracker);
+
+    /**
+     * @param iTrackingTool $tool
+     * @param bool $doFlush
+     * @return mixed
+     */
+    public function remove(iTrackingTool $tool, $doFlush = true);
+}
