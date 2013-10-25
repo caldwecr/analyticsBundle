349b4b0837d2a34d711487fcceb31702c8cbeee9
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-25 11:25:40 -0500

    Added service interface type

---
+++ Services/iTrackingToolValidator.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/25/13
+ * Time: 11:25 AM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services;
+
+use Cympel\Bundle\AnalyticsBundle\Entity\iType;
+
+interface iTrackingToolValidator extends iType
+{
+
+}
