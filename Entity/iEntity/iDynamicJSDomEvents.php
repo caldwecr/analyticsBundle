1de18cbeaca166053d8184bb22672630d66c1542
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-28 14:49:03 -0500

    Added the iDynamicJSDomEvents interface

---
+++ Entity/iEntity/iDynamicJSDomEvents.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/28/13
+ * Time: 2:48 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity\iEntity;
+
+use Cympel\Bundle\AnalyticsBundle\Entity\iType;
+
+interface iDynamicJSDomEvents extends iType
+{
+
+}
