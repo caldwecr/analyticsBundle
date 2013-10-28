8dde89875335fd8f4f2fa26318e524f82e18537a
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-28 14:13:14 -0500

    Created the DynamicJSSelectorsRemover service and added it to the service config

---
+++ Services/DynamicJSSelectors/DynamicJSSelectorsRemover.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/28/13
+ * Time: 2:07 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services\DynamicJSSelectors;
+
+use Cympel\Bundle\AnalyticsBundle\Entity\DynamicJSSelectors;
+use Cympel\Bundle\AnalyticsBundle\Services\iServices\iDynamicJSSelectorsRemover;
+
+class DynamicJSSelectorsRemover implements iDynamicJSSelectorsRemover
+{
+    /**
+     * @var Object - the doctrine service
+     */
+    protected $doctrine;
+
+    /**
+     * @var string - the entity manager to use for ORM
+     */
+    protected $emName;
+
+    /**
+     * @param Object $doctrine
+     * @param string $entityManagerName
+     */
+    public function __construct($doctrine, $entityManagerName)
+    {
+        $this->doctrine = $doctrine;
+        $this->emName = $entityManagerName;
+    }
+
+    /**
+     * @param DynamicJSSelectors $selectors
+     * @return void
+     */
+    public function remove(DynamicJSSelectors $selectors)
+    {
+        $em = $this->doctrine->getManager($this->emName);
+        $em->remove($selectors);
+        $em->flush();
+    }
+
+    /**
+     * @return string
+     * This method must return a string with a unique representation of the object type that is implementing this interface
+     */
+    public function getType()
+    {
+        return 'DynamicJSSelectorsRemover';
+    }
+
+}
