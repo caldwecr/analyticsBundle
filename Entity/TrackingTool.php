0f2793f494b8976eb71540b429f00324b8e45e2b
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-28 18:58:15 -0500

    Added new abstract class TrackingTool

---
+++ Entity/TrackingTool.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/28/13
+ * Time: 6:57 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity;
+
+abstract class TrackingTool extends CympelType implements iTrackingTool
+{
+
+}
