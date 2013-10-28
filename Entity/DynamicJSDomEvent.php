79bf9627ea118ffb79320e4b777784acfd13e196
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-28 18:38:52 -0500

    Updated the DynamicJSDomEvent entity with appropriate ORM configuration

--- Entity/DynamicJSDomEvent.php
+++ Entity/DynamicJSDomEvent.php
@@ -10,21 +10,34 @@ namespace Cympel\Bundle\AnalyticsBundle\Entity;
 
 use Cympel\Bundle\AnalyticsBundle\Entity\iEntity\iDynamicJSDomEvent;
 use Cympel\Bundle\AnalyticsBundle\Entity\iEntity\iDynamicJSDomEvents;
+use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * Class DynamicJSDomEvent
+ * @package Cympel\Bundle\AnalyticsBundle\Entity
+ * @ORM\Entity
+ * @ORM\Table(name="DynamicJSDomEvent")
+ */
 class DynamicJSDomEvent extends CympelType implements iDynamicJSDomEvent
 {
     /**
      * @var int
+     * @ORM\Id
+     * @ORM\Column(type="bigint")
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
 
     /**
      * @var string
+     * @ORM\Column(type="string", length=50)
      */
     protected $eventName;
 
     /**
      * @var iDynamicJSDomEvents
+     * @ORM\ManyToOne(targetEntity="DynamicJSDomEvents", inversedBy="events", cascade={"persist"})
+     * @ORM\JoinColumn(name="parentDynamicJSDomEventsId", referencedColumnName="id")
      */
     protected $parentDynamicJSDomEvents;
 
@@ -35,6 +48,54 @@ class DynamicJSDomEvent extends CympelType implements iDynamicJSDomEvent
     public function getType()
     {
         return 'DynamicJSDomEvent';
+    }
+
+    /**
+     * @param string $eventName
+     */
+    public function setEventName($eventName)
+    {
+        $this->eventName = $eventName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEventName()
+    {
+        return $this->eventName;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param \Cympel\Bundle\AnalyticsBundle\Entity\iEntity\iDynamicJSDomEvents $parentDynamicJSDomEvents
+     */
+    public function setParentDynamicJSDomEvents($parentDynamicJSDomEvents)
+    {
+        $this->parentDynamicJSDomEvents = $parentDynamicJSDomEvents;
+    }
+
+    /**
+     * @return \Cympel\Bundle\AnalyticsBundle\Entity\iEntity\iDynamicJSDomEvents
+     */
+    public function getParentDynamicJSDomEvents()
+    {
+        return $this->parentDynamicJSDomEvents;
     }
 
     /**
