969c5ac0038fa3b3c606cdb38197928f8d8b7f94
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-28 15:05:44 -0500

    Added the base abstract class CympelType to avoid so much rework

---
+++ Entity/CympelType.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/28/13
+ * Time: 3:03 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity;
+
+use Cympel\Bundle\AnalyticsBundle\Entity\Exception\TypeMismatchException;
+
+abstract class CympelType implements iType
+{
+    /**
+     * @param iType $rightSide
+     * @throws TypeMismatchException
+     * @return bool
+     *
+     * This method should first compare an objects type to the arguments type, if they do not match to method should return throw a TypeMismatchException
+     * containing the string "types do not match: leftside = ..., rightside = ..."
+     * Otherwise return true if they are equals otherwise false
+     */
+    public function equals(iType $rightSide)
+    {
+        $leftSideType = $this->getType();
+        $rightSideType = $rightSide->getType();
+        if($leftSideType !== $rightSideType) {
+            throw new TypeMismatchException("types do not match: leftside = {$leftSideType}, rightside = {$rightSideType}");
+        }
+        return $this->typedEquals($rightSide);
+    }
+
+    /**
+     * @param iType $rightSide
+     * @return bool
+     */
+    abstract protected function typedEquals(iType $rightSide);
+}
