fc2f623c5042bb337d0f09393cd592487b5224d7
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-28 10:33:55 -0500

    Created the DynamicJSSelector entity class

---
+++ Entity/DynamicJSSelector.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/28/13
+ * Time: 10:10 AM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Class DynamicJSSelector
+ * @package Cympel\Bundle\AnalyticsBundle\Entity
+ * @ORM\Entity
+ * @ORM\Table(name="DynamicJSSelector")
+ */
+class DynamicJSSelector implements iTrackingToolSelector
+{
+    /**
+     * @var int
+     * @ORM\Id
+     * @ORM\Column(type="bigint")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Range(min=2, max=255)
+     */
+    protected $selection;
+
+    /**
+     * @var DynamicJSSelectors
+     * @ORM\ManyToOne(targetEntity="DynamicJSSelectors", inversedBy="selectors")
+     * @ORM\JoinColumn(name="selectorsId", referencedColumnName="id")
+     */
+    protected $parentSelectors;
+
+
+    /**
+     * @return string
+     * This method must return a string with a unique representation of the object type that is implementing this interface
+     */
+    public function getType()
+    {
+        return 'Selector';
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return void
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSelection()
+    {
+        return $this->selection;
+    }
+
+    /**
+     * @param string $selection
+     * @return void
+     */
+    public function setSelection($selection)
+    {
+        $this->selection = $selection;
+    }
+
+    /**
+     * @return DynamicJSSelectors
+     */
+    public function getParentSelectors()
+    {
+        return $this->parentSelectors;
+    }
+
+    /**
+     * @param DynamicJSSelectors $selectors
+     * @return void
+     */
+    public function setParentSelectors(DynamicJSSelectors $selectors)
+    {
+        $this->parentSelectors = $selectors;
+    }
+}
