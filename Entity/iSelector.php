4322b8b0046b826375591fcd48dc2228c4fabc5c
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-28 10:16:43 -0500

    Added iSelector interface

---
+++ Entity/iSelector.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/28/13
+ * Time: 10:14 AM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity;
+
+/**
+ * Interface iSelector
+ * @package Cympel\Bundle\AnalyticsBundle\Entity
+ */
+interface iSelector extends iType
+{
+    /**
+     * @return int
+     */
+    public function getId();
+
+    /**
+     * @param int $id
+     * @return void
+     */
+    public function setId($id);
+
+    /**
+     * @return string
+     */
+    public function getSelection();
+
+    /**
+     * @param string $selection
+     * @return void
+     */
+    public function setSelection($selection);
+}
