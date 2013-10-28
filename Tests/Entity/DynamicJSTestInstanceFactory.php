d4c50022f513d2b92c8a2e64b562d017b162184d
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-28 12:40:12 -0500

    Created test instance factory for DynamicJS entities

---
+++ Tests/Entity/DynamicJSTestInstanceFactory.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/28/13
+ * Time: 12:36 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Tests\Entity;
+
+use Cympel\Bundle\AnalyticsBundle\Entity\DynamicJS;
+use Cympel\Bundle\AnalyticsBundle\Entity\Tracker;
+use Doctrine\Common\Collections\ArrayCollection;
+
+class DynamicJSTestInstanceFactory
+{
+    public static function generate()
+    {
+        $t = new Tracker();
+        $i = new DynamicJS();
+        $ac = new ArrayCollection(array($i));
+        $t->setTrackingTools($ac);
+        return $i;
+    }
+}
