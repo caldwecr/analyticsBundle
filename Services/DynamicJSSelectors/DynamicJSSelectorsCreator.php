a4a970ceee2c386ff63154cd79ef0987b6b0ac98
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-28 13:41:18 -0500

    Added the DynamicJSSelectorsCreator service and added it to the service config

---
+++ Services/DynamicJSSelectors/DynamicJSSelectorsCreator.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/28/13
+ * Time: 1:38 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services\Selectors;
+
+use Cympel\Bundle\AnalyticsBundle\Entity\DynamicJSSelectors;
+use Cympel\Bundle\AnalyticsBundle\Services\iServices\iDynamicJSSelectorsCreator;
+
+class DynamicJSSelectorsCreator implements iDynamicJSSelectorsCreator
+{
+    /**
+     * @return DynamicJSSelectors
+     */
+    public function create()
+    {
+        return new DynamicJSSelectors();
+    }
+
+    /**
+     * @return string
+     * This method must return a string with a unique representation of the object type that is implementing this interface
+     */
+    public function getType()
+    {
+        return 'DynamicJSSelectorsCreator';
+    }
+
+}
