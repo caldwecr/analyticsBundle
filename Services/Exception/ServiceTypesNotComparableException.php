207267d62c0f0b42a728a9f5055b31da0765832d
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-28 19:16:09 -0500

    Created the ServiceTypesNotComparableException

---
+++ Services/Exception/ServiceTypesNotComparableException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/28/13
+ * Time: 7:15 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services\Exception;
+
+class ServiceTypesNotComparableException extends \Exception
+{
+
+}
