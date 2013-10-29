5bbccc2ddae0d9173d49d28aa942df501ddca768
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-29 10:39:47 -0500

    Updated to work with generics

--- Tests/Services/DynamicCSSManagerRenderDCSSByIdTest.php
+++ Tests/Services/DynamicCSSManagerRenderDCSSByIdTest.php
@@ -20,15 +20,15 @@ class DynamicCSSManagerRenderDCSSByIdTest extends ContainerAwareUnitTestCase
             'hoo'
         );
         $pseudo = 'visited';
-        $uri = $dcm->generateOneTimeStylesheet($ids, $pseudo);
+        $uri = $dcm->generateOneTimeStylesheet('DynamicCSS', $ids, $pseudo);
 
         $uri_prefix = '/analytics/dynamic/css/';
         $this->assertTrue(strpos($uri, $uri_prefix) !== false);
         $id = str_replace($uri_prefix, '', $uri);
 
-        $dcm->renderById($id);
+        $dcm->renderById('DynamicCSS', $id);
 
-        $dcss = $dcm->findOneTimeStylesheetById($id);
+        $dcss = $dcm->findOneTimeStylesheetById('DynamicCSS', $id);
         $this->assertNotEquals(0, $dcss->getRendered());
     }
 }
