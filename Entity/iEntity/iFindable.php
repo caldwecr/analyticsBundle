1dbfeb8dde999392da4c84bc8e778700ab371430
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-28 21:26:05 -0500

    Created the iFindable interface

---
+++ Entity/iEntity/iFindable.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/28/13
+ * Time: 9:22 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity\iEntity;
+
+use Cympel\Bundle\AnalyticsBundle\Entity\iType;
+
+interface iFindable extends iType
+{
+    /**
+     * @return string
+     *
+     * This method must return the fully qualified repository name
+     */
+    public function getRepositoryName();
+}
