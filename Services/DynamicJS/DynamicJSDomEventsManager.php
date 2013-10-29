bc0cdc3d00efb539a451179c4ca1f69dbd4e380f
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-29 15:13:54 -0500

    First real attempt at implementing this service

--- Services/DynamicJS/DynamicJSDomEventsManager.php
+++ Services/DynamicJS/DynamicJSDomEventsManager.php
@@ -8,12 +8,14 @@
  */
 namespace Cympel\Bundle\AnalyticsBundle\Services\DynamicJS;
 
+use Cympel\Bundle\AnalyticsBundle\Entity\iEntity\iDynamicJSDomEvents;
 use Cympel\Bundle\AnalyticsBundle\Services\CympelService;
 use Cympel\Bundle\AnalyticsBundle\Services\iServices\iCreator;
 use Cympel\Bundle\AnalyticsBundle\Services\iServices\iDynamicJSDomEventsManager;
 use Cympel\Bundle\AnalyticsBundle\Services\iServices\iFinder;
 use Cympel\Bundle\AnalyticsBundle\Services\iServices\iPersister;
 use Cympel\Bundle\AnalyticsBundle\Services\iServices\iRemover;
+use Cympel\Bundle\AnalyticsBundle\Services\iServices\iDynamicJSDomEventManager;
 
 class DynamicJSDomEventsManager extends CympelService implements iDynamicJSDomEventsManager
 {
@@ -38,17 +40,24 @@ class DynamicJSDomEventsManager extends CympelService implements iDynamicJSDomEv
     protected $remover;
 
     /**
+     * @var iDynamicJSDomEventManager
+     */
+    protected $dynamicJDomEventManager;
+
+    /**
      * @param iCreator $creator
      * @param iFinder $finder
      * @param iPersister $persister
      * @param iRemover $remover
+     * @param iDynamicJSDomEventManager $dynamicJDomEventManager
      */
-    public function __construct(iCreator $creator, iFinder $finder, iPersister $persister, iRemover $remover)
+    public function __construct(iCreator $creator, iFinder $finder, iPersister $persister, iRemover $remover, iDynamicJSDomEventManager $dynamicJDomEventManager)
     {
         $this->creator = $creator;
         $this->finder = $finder;
         $this->persister = $persister;
         $this->remover = $remover;
+        $this->dynamicJDomEventManager = $dynamicJDomEventManager;
     }
 
     /**
@@ -92,4 +101,18 @@ class DynamicJSDomEventsManager extends CympelService implements iDynamicJSDomEv
         return 'DynamicJSDomEventsManager';
     }
 
+    /**
+     * @param array $eventsArray
+     * @return iDynamicJSDomEvents
+     */
+    public function createFromArray($eventsArray)
+    {
+        $events = $this->creator->create('DynamicJSDomEvents');
+        foreach($eventsArray as $key => $value) {
+            $events[$key] = $this->dynamicJDomEventManager->getCreator()->create('DynamicJSDomEvent');
+            $events[$key]->setEventName($value);
+            $events[$key]->setParentDynamicJDomEvents($events);
+        }
+        return $events;
+    }
 }
