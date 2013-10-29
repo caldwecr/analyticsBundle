a74359a498594ceffd5de27cf319b919367bc78e
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-28 21:29:15 -0500

    Added the iFinder interface

---
+++ Services/iServices/iFinder.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/28/13
+ * Time: 9:28 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services\iServices;
+
+use Cympel\Bundle\AnalyticsBundle\Entity\iType;
+use Cympel\Bundle\AnalyticsBundle\Entity\iEntity\iFindable;
+
+interface iFinder extends iType
+{
+    /**
+     * @param string $id
+     * @param string $classAlias
+     * @return iFindable
+     */
+    public function findOneByIdAndClassAlias($id, $classAlias);
+}
