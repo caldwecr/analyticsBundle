dde4d8f2729caf0e5125a7d2b6e942a061faf6fc
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-28 20:39:18 -0500

    Added interface iRemover

---
+++ Services/iServices/iRemover.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/28/13
+ * Time: 8:36 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services\iServices;
+
+use Cympel\Bundle\AnalyticsBundle\Entity\iType;
+use Cympel\Bundle\AnalyticsBundle\Entity\iEntity\iRemovable;
+
+interface iRemover extends iType
+{
+    /**
+     * @param iRemovable $removable
+     * @return void
+     */
+    public function remove(iRemovable $removable);
+}
