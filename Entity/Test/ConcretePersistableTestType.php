ab15f261826f7a8e8d73d66b4bdc1a588be5d603
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-28 20:08:17 -0500

    Created the ConcretePersistableTestType for use in testing services configuration

---
+++ Entity/Test/ConcretePersistableTestType.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/28/13
+ * Time: 7:59 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity\Test;
+
+use Cympel\Bundle\AnalyticsBundle\Entity\CympelType;
+use Cympel\Bundle\AnalyticsBundle\Entity\iEntity\iPersistable;
+use Cympel\Bundle\AnalyticsBundle\Entity\iType;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class ConcretePersistableTestType
+ * @package Cympel\Bundle\AnalyticsBundle\Entity\Test
+ * @ORM\Entity
+ * @ORM\Table(name="ConcretePersistableTestType")
+ */
+class ConcretePersistableTestType extends CympelType implements iPersistable
+{
+    protected $entityManagerName;
+
+    /**
+     * @var int
+     * @ORM\Id
+     * @ORM\Column(type="bigint")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var int
+     * @ORM\Column(type="string", length=10)
+     */
+    protected $value;
+
+    /**
+     * @param iType $rightSide
+     * @return bool
+     *
+     * Note that the object type passed into this method will always match the class type where this method is implemented.
+     */
+    protected function typedEquals(iType $rightSide)
+    {
+        return self::areEqual($this, $rightSide);
+    }
+
+    /**
+     * @return string
+     */
+    public function getEntityManagerName()
+    {
+        return $this->entityManagerName;
+    }
+
+    /**
+     * @param string $entityManagerName
+     */
+    public function setEntityManagerName($entityManagerName)
+    {
+        $this->entityManagerName = $entityManagerName;
+    }
+
+    /**
+     * @return string
+     * This method must return a string with a unique representation of the object type that is implementing this interface
+     */
+    public function getType()
+    {
+        return 'ConcretePersistableTestType';
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $value
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return int
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+}
