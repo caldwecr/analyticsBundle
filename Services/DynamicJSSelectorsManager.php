af6f35eb5d3fa1095a6655011a451816ae8ca2f5
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-29 10:52:22 -0500

    Created the DynamicJSSelectorsManager service

---
+++ Services/DynamicJSSelectorsManager.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/29/13
+ * Time: 10:49 AM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services;
+
+use Cympel\Bundle\AnalyticsBundle\Services\iServices\iCreator;
+use Cympel\Bundle\AnalyticsBundle\Services\iServices\iDynamicJSSelectorsManager;
+use Cympel\Bundle\AnalyticsBundle\Services\iServices\iFinder;
+use Cympel\Bundle\AnalyticsBundle\Services\iServices\iPersister;
+use Cympel\Bundle\AnalyticsBundle\Services\iServices\iRemover;
+
+class DynamicJSSelectorsManager extends CympelService implements iDynamicJSSelectorsManager
+{
+    /**
+     * @var iCreator
+     */
+    protected $creator;
+
+    /**
+     * @var iFinder
+     */
+    protected $finder;
+
+    /**
+     * @var iPersister
+     */
+    protected $persister;
+
+    /**
+     * @var iRemover
+     */
+    protected $remover;
+
+    /**
+     * @param iCreator $creator
+     * @param iFinder $finder
+     * @param iPersister $persister
+     * @param iRemover $remover
+     */
+    public function __construct(iCreator $creator, iFinder $finder, iPersister $persister, iRemover $remover)
+    {
+        $this->creator = $creator;
+        $this->finder = $finder;
+        $this->persister = $persister;
+        $this->remover = $remover;
+    }
+
+    /**
+     * @return iCreator
+     */
+    public function getCreator()
+    {
+        return $this->creator;
+    }
+
+    /**
+     * @return iFinder
+     */
+    public function getFinder()
+    {
+        return $this->finder;
+    }
+
+    /**
+     * @return iPersister
+     */
+    public function getPersister()
+    {
+        return $this->persister;
+    }
+
+    /**
+     * @return iRemover
+     */
+    public function getRemover()
+    {
+        return $this->remover;
+    }
+
+    /**
+     * @return string
+     * This method must return a string with a unique representation of the object type that is implementing this interface
+     */
+    public function getType()
+    {
+        // TODO: Implement getType() method.
+    }
+
+}
