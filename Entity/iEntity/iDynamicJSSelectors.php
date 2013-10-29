ac2944dc588c028136d180dbc7a1e6d12a4b6361
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-29 14:55:13 -0500

    Added the iDynamicJSSelectors interface

---
+++ Entity/iEntity/iDynamicJSSelectors.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/29/13
+ * Time: 2:54 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity\iEntity;
+
+interface iDynamicJSSelectors extends iCreatable, iPersistable, iFindable, iRemovable
+{
+
+}
