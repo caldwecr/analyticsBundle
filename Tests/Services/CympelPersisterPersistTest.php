b2abb4b734772f6f72ab785e188beeb8f8c86cab
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-29 09:42:16 -0500

    Resolved typo in service name

--- Tests/Services/CympelPersisterPersistTest.php
+++ Tests/Services/CympelPersisterPersistTest.php
@@ -15,7 +15,7 @@ class CympelPersisterPersistTest extends ContainerAwareUnitTestCase
 {
     public function testPersist()
     {
-        $persister = $this->get('cympel_anayltics.generics.persister');
+        $persister = $this->get('cympel_analytics.generics.persister');
         $cp = new ConcretePersistableTestType();
         $cp->setEntityManagerName('cympelanalytics');
         $cp->setValue("foobarz");
