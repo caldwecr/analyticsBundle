e0c3f4537d7d1d04edb87dd7e48a1eb90c431862
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-29 14:39:46 -0500

    Good for now - coming back later based on todos to write additional tests

--- Services/DynamicJS/DynamicJSSelectorsManager.php
+++ Services/DynamicJS/DynamicJSSelectorsManager.php
@@ -10,10 +10,12 @@ namespace Cympel\Bundle\AnalyticsBundle\Services\DynamicJS;
 
 use Cympel\Bundle\AnalyticsBundle\Services\CympelService;
 use Cympel\Bundle\AnalyticsBundle\Services\iServices\iCreator;
+use Cympel\Bundle\AnalyticsBundle\Services\iServices\iDynamicJSSelectorManager;
 use Cympel\Bundle\AnalyticsBundle\Services\iServices\iDynamicJSSelectorsManager;
 use Cympel\Bundle\AnalyticsBundle\Services\iServices\iFinder;
 use Cympel\Bundle\AnalyticsBundle\Services\iServices\iPersister;
 use Cympel\Bundle\AnalyticsBundle\Services\iServices\iRemover;
+use Cympel\Bundle\AnalyticsBundle\Entity\DynamicJSSelectors;
 
 class DynamicJSSelectorsManager extends CympelService implements iDynamicJSSelectorsManager
 {
@@ -38,17 +40,41 @@ class DynamicJSSelectorsManager extends CympelService implements iDynamicJSSelec
     protected $remover;
 
     /**
+     * @var iDynamicJSSelectorManager
+     */
+    protected $selectorManager;
+
+    /**
      * @param iCreator $creator
      * @param iFinder $finder
      * @param iPersister $persister
      * @param iRemover $remover
+     * @param iDynamicJSSelectorManager $selectorManager
      */
-    public function __construct(iCreator $creator, iFinder $finder, iPersister $persister, iRemover $remover)
+    public function __construct(iCreator $creator, iFinder $finder, iPersister $persister, iRemover $remover, iDynamicJSSelectorManager $selectorManager)
     {
         $this->creator = $creator;
         $this->finder = $finder;
         $this->persister = $persister;
         $this->remover = $remover;
+        $this->selectorManager = $selectorManager;
+    }
+
+    /**
+     * @param $selectorArray
+     * @return DynamicJSSelectors
+     */
+    public function createFromArray($selectorArray)
+    {
+        $selectors = $this->creator->create('DynamicJSSelectors');
+        foreach($selectorArray as $key => $value) {
+            $selectors[$key] = $this->selectorManager->getCreator()->create('DynamicJSSelector');
+            $selectors[$key]->setSelection($value);
+            $selectors[$key]->setParentSelectors($selectors);
+            $selectors[$key]->setCreated(time());
+            $selectors[$key]->setCalled(0);
+        }
+        return $selectors;
     }
 
     /**
