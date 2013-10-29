bd1fa858fc53e1f7005d02a864257772b04a0378
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-28 19:03:23 -0500

    Added abstract class PropertySet

---
+++ Entity/PropertySet.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/28/13
+ * Time: 7:02 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity;
+
+abstract class PropertySet extends CympelType implements iPropertySet
+{
+
+}
