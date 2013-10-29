3bbb26c296ea200eea8aa9b80aceff93ecc628fe
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-29 15:28:44 -0500

    Updated interface signature

--- Entity/iEntity/iDynamicJSDomEvent.php
+++ Entity/iEntity/iDynamicJSDomEvent.php
@@ -8,7 +8,7 @@
  */
 namespace Cympel\Bundle\AnalyticsBundle\Entity\iEntity;
 
-interface iDynamicJSDomEvent extends iDomEvent
+interface iDynamicJSDomEvent extends iDomEvent, iPersistable, iCreatable, iRemovable, iFindable
 {
 
 }
