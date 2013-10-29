80d6d03d0eb8053b37d475ea6a62767dde1c850d
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-28 20:38:53 -0500

    Added interface iRemovable

---
+++ Entity/iEntity/iRemovable.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/28/13
+ * Time: 8:37 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity\iEntity;
+
+interface iRemovable extends iPersistable
+{
+
+}
