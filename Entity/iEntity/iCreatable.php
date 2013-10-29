44aac66992c11c10c2cd700b29d5377bfd8f08ff
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-28 19:38:12 -0500

    Added the iCreatable interface

---
+++ Entity/iEntity/iCreatable.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/28/13
+ * Time: 7:35 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity\iEntity;
+
+interface iCreatable
+{
+    /**
+     * @param string $repositoryName
+     * @return void
+     */
+    public function setRepositoryName($repositoryName);
+
+    /**
+     * @param string $entityManagerName
+     * @return void
+     */
+    public function setEntityManagerName($entityManagerName);
+}
