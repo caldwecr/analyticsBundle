9e9fa671758fd0b629bfaf5d00603d9b650a09b6
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-30 14:54:10 -0500

    Added the iManager interface

---
+++ Services/iServices/iManager.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/30/13
+ * Time: 2:53 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services\iServices;
+
+interface iManager extends iCreate, iFind, iPersist, iRemove
+{
+
+}
