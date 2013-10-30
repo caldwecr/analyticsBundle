8ccf22752885cbffbb0b8c094333b48891e5757e
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-29 21:21:46 -0500

    Added the iService interface

---
+++ Services/iServices/iService.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/29/13
+ * Time: 9:21 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services\iServices;
+
+use Cympel\Bundle\AnalyticsBundle\Entity\iType;
+
+interface iService extends iType
+{
+
+}
