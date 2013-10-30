39ee63ef0fe8937f30363afb052fa2e937bf7f31
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-29 20:48:01 -0500

    Updated based on new folder structure for DynamicJS

--- Tests/Entity/DynamicJSSelectorsTestInstanceFactory.php
+++ Tests/Entity/DynamicJSSelectorsTestInstanceFactory.php
@@ -8,7 +8,7 @@
  */
 namespace Cympel\Bundle\AnalyticsBundle\Tests\Entity;
 
-use Cympel\Bundle\AnalyticsBundle\Entity\DynamicJSSelectors;
+use Cympel\Bundle\AnalyticsBundle\Entity\DynamicJS\DynamicJSSelectors;
 
 class DynamicJSSelectorsTestInstanceFactory
 {
