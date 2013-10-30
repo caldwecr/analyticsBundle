13f40ea3f08515841773f002d8381d2c564ff801
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-29 21:11:01 -0500

    Still working on the callbackAction

--- Controller/DynamicJSController.php
+++ Controller/DynamicJSController.php
@@ -11,6 +11,7 @@ namespace Cympel\Bundle\AnalyticsBundle\Controller;
 
 use Cympel\Bundle\AnalyticsBundle\Entity\DynamicCSS;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Response;
 
 
 class DynamicJSController extends Controller
@@ -27,15 +28,39 @@ class DynamicJSController extends Controller
         $selectors = $djm->getDynamicJSelectors($dj);
 
         $events = $djm->getDynamicJDomEvents($dj);
-
-        return $this->render('CympelAnalyticsBundle:DynamicJS:djs.js.twig', array(
+        $selectorList = '';
+        $isFirst = true;
+        foreach($selectors as $key => $value) {
+            if(!$isFirst) {
+                $selectorList .= ', ';
+            } else {
+                $isFirst = false;
+            }
+            $selectorList .= $value->getSelection();
+        }
+        $callbackUrl = $this->generateUrl('dynamicJSCallbackShortUrl', array(
+            'key' => $key,
+            'selectorKey' => 'sKS', // This literal 'sKS' and 'eKS' below are matched by the javascript on the client for routing
+            'eventKey' => 'eKS',
+        ));
+        $response =  $this->render('CympelAnalyticsBundle:DynamicJS:djs.js.twig', array(
             'selectors' => $selectors,
             'events' => $events,
+            'callbackUrl' => $callbackUrl,
         ));
+        $response->headers->set('Content-Type', 'text/javascript');
+        return $response;
     }
 
-    public function dynamicJSCallbackAction()
+    public function dynamicJSCallbackAction($key, $selectorKey, $eventKey)
     {
-        //return a message received / success message
+        //generate a selectorevent entity and persist it
+
+        //make sure the rendered property of the DynamicJS is set
+
+        //make sure the called property of the DynamicJSSelector is set
+
+        //return a response
+        return new Response();
     }
 }
