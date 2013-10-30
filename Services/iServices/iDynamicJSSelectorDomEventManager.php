0b9d4038a8d9b9a37d1b83dc1c2c18bae1cfa935
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-29 21:27:31 -0500

    Added the iDynamicJSSelectorDomEventManager interface

---
+++ Services/iServices/iDynamicJSSelectorDomEventManager.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/29/13
+ * Time: 9:16 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services\iServices;
+
+interface iDynamicJSSelectorDomEventManager extends iCreate, iPersist, iFind, iRemove
+{
+
+}
