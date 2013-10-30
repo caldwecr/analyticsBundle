a359fabf70029ef9e20be8fe6b99dcb77bad550b
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-30 12:57:57 -0500

    Added the iCreatableAndFindable interface

---
+++ Entity/iEntity/iCreatableAndFindable.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/30/13
+ * Time: 12:57 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity\iEntity;
+
+interface iCreatableAndFindable extends iFindable, iCreatable
+{
+
+}
