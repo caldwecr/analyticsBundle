fa9b921a7f071ff16af554b61c4da64682c521dd
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-30 13:37:03 -0500

    Added the iDynamicJSSelectorDomEventClientData interface

---
+++ Entity/iEntity/iDynamicJSSelectorDomEventClientData.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/30/13
+ * Time: 1:36 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity\iEntity;
+
+interface iDynamicJSSelectorDomEventClientData extends iFindable, iPersistable, iRemovable, iCreatable
+{
+
+}
