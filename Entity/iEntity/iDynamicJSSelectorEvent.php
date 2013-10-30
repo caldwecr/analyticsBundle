d4e53587f9cd11fe25967eaa0d87a00e9a8fc0b2
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-29 20:10:24 -0500

    Created the iDynamicJSSelectorEvent interface

---
+++ Entity/iEntity/iDynamicJSSelectorEvent.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/29/13
+ * Time: 8:09 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity\iEntity;
+
+interface iDynamicJSSelectorEvent extends iFindable, iPersistable, iRemovable, iCreatable
+{
+
+}
