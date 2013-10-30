abe2cfa4613b75c27cbfd098172684071303918e
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-29 20:30:17 -0500

    Added the iDynamicJS interface

---
+++ Entity/iEntity/iDynamicJS.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/29/13
+ * Time: 8:29 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity\iEntity;
+
+interface iDynamicJS extends iCreatable, iPersistable, iFindable, iRemovable
+{
+
+}
