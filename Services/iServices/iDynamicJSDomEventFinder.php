e62a79e7089c6554ceacc86c7fca4bdf3970bf79
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-10-30 12:35:27 -0500

    Added the iDynamicJSDomEventFinder interface, which extends the iFinder interface

---
+++ Services/iServices/iDynamicJSDomEventFinder.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 10/30/13
+ * Time: 12:33 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services\iServices;
+
+use Cympel\Bundle\AnalyticsBundle\Entity\iEntity\iDynamicJSDomEvents;
+use Cympel\Bundle\AnalyticsBundle\Entity\iEntity\iDynamicJSDomEvent;
+
+/**
+ * Interface iDynamicJSDomEventFinder
+ * @package Cympel\Bundle\AnalyticsBundle\Services\iServices
+ */
+interface iDynamicJSDomEventFinder extends iFinder
+{
+    /**
+     * @param iDynamicJSDomEvents $domEvents
+     * @param $eventName
+     * @return iDynamicJSDomEvent
+     */
+    public function findOneEventByEventsAndEventName(iDynamicJSDomEvents $domEvents, $eventName);
+}
