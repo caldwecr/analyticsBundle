bd6dd405a2b5abf169bd3f8f6e79749bce7f7ca2
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-01 14:21:13 -0500

    Added the iCympelRouterService interface

---
+++ Services/iServices/iCympelRouterService.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 11/1/13
+ * Time: 2:20 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services\iServices;
+
+interface iCympelRouterService extends iService
+{
+
+}
