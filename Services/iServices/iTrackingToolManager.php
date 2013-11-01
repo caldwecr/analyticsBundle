59643120aac5966376af5c3a628d7c15de2f6488
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-01 15:02:07 -0500

    Added the createTrackingTool method to the iTrackingToolManager interface

--- Services/iServices/iTrackingToolManager.php
+++ Services/iServices/iTrackingToolManager.php
@@ -17,56 +17,6 @@ use Cympel\Bundle\AnalyticsBundle\Entity\iType;
 interface iTrackingToolManager extends iType, iValidate, iPersist, iFind, iRemove, iCreate
 {
     /**
-     * @param string $classAlias
-     * @param iTracker $tracker
-     * @return iTrackingTool
-     *
-     * This method creates a new tracking tool that is the descendant of the first argument
-     */
-    public function create($classAlias, iTracker $tracker = null);
-
-    /**
-     * @param iTrackingTool $tool
-     * @throws \Cympel\Bundle\AnalyticsBundle\Entity\Exception\InvalidTrackingToolException
-     * @return bool
-     *
-     * This method should persist a tracking tool to the database
-     */
-    public function persist(iTrackingTool $tool);
-
-    /**
-     * @param iTrackingTool $tool
-     * @throws \Cympel\Bundle\AnalyticsBundle\Entity\Exception\InvalidTrackingToolException
-     * @return bool
-     *
-     * This method should remove a tracking tool from the database
-     */
-    public function remove(iTrackingTool $tool);
-
-
-    /**
-     * @param $id
-     * @param $classAlias
-     * @return iFindable
-     */
-    public function findOneByIdAndClassAlias($id, $classAlias);
-
-    /**
-     * @param $entityManagerName
-     * @return void
-     *
-     * This method must set the manager's entity manager name property
-     */
-    public function setEntityManagerName($entityManagerName);
-
-    /**
-     * @return string
-     *
-     * This method must return the manager's entity manager name
-     */
-    public function getEntityManagerName();
-
-    /**
      * @param iTrackingTool $tool
      * @param iPropertySet $properties
      * @return iTrackingTool
@@ -80,4 +30,11 @@ interface iTrackingToolManager extends iType, iValidate, iPersist, iFind, iRemov
      * This method must return all bindings on the tracking tool
      */
     public function getProperties(iTrackingTool $tool);
+
+    /**
+     * @param string $classAlias
+     * @param iTracker $tracker
+     * @return iTrackingTool
+     */
+    public function createTrackingTool($classAlias, iTracker $tracker);
 }
