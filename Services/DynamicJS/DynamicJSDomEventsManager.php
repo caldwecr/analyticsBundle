b88eafe97d8e1f1dad8fcc8a46d2e60468e7a636
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-01 11:22:06 -0500

    Updated the DynamicJSDomEventsManager to extend CympelManager and reduce duplicate code. Updated services configuration to work with this change.

--- Services/DynamicJS/DynamicJSDomEventsManager.php
+++ Services/DynamicJS/DynamicJSDomEventsManager.php
@@ -8,90 +8,14 @@
  */
 namespace Cympel\Bundle\AnalyticsBundle\Services\DynamicJS;
 
+use Cympel\Bundle\AnalyticsBundle\Entity\iEntity\iDynamicJSDomEvent;
 use Cympel\Bundle\AnalyticsBundle\Entity\iEntity\iDynamicJSDomEvents;
-use Cympel\Bundle\AnalyticsBundle\Services\CympelService;
-use Cympel\Bundle\AnalyticsBundle\Services\iServices\iCreator;
+use Cympel\Bundle\AnalyticsBundle\Services\CympelManager;
 use Cympel\Bundle\AnalyticsBundle\Services\iServices\iDynamicJSDomEventsManager;
-use Cympel\Bundle\AnalyticsBundle\Services\iServices\iFinder;
-use Cympel\Bundle\AnalyticsBundle\Services\iServices\iPersister;
-use Cympel\Bundle\AnalyticsBundle\Services\iServices\iRemover;
-use Cympel\Bundle\AnalyticsBundle\Services\iServices\iDynamicJSDomEventManager;
 use Doctrine\Common\Collections\ArrayCollection;
 
-class DynamicJSDomEventsManager extends CympelService implements iDynamicJSDomEventsManager
+class DynamicJSDomEventsManager extends CympelManager implements iDynamicJSDomEventsManager
 {
-    /**
-     * @var iCreator
-     */
-    protected $creator;
-
-    /**
-     * @var iFinder
-     */
-    protected $finder;
-
-    /**
-     * @var iPersister
-     */
-    protected $persister;
-
-    /**
-     * @var iRemover
-     */
-    protected $remover;
-
-    /**
-     * @var iDynamicJSDomEventManager
-     */
-    protected $dynamicJDomEventManager;
-
-    /**
-     * @param iCreator $creator
-     * @param iFinder $finder
-     * @param iPersister $persister
-     * @param iRemover $remover
-     * @param iDynamicJSDomEventManager $dynamicJDomEventManager
-     */
-    public function __construct(iCreator $creator, iFinder $finder, iPersister $persister, iRemover $remover, iDynamicJSDomEventManager $dynamicJDomEventManager)
-    {
-        $this->creator = $creator;
-        $this->finder = $finder;
-        $this->persister = $persister;
-        $this->remover = $remover;
-        $this->dynamicJDomEventManager = $dynamicJDomEventManager;
-    }
-
-    /**
-     * @return iCreator
-     */
-    public function getCreator()
-    {
-        return $this->creator;
-    }
-
-    /**
-     * @return iFinder
-     */
-    public function getFinder()
-    {
-        return $this->finder;
-    }
-
-    /**
-     * @return iPersister
-     */
-    public function getPersister()
-    {
-        return $this->persister;
-    }
-
-    /**
-     * @return iRemover
-     */
-    public function getRemover()
-    {
-        return $this->remover;
-    }
 
     /**
      * @return string
@@ -111,11 +35,21 @@ class DynamicJSDomEventsManager extends CympelService implements iDynamicJSDomEv
         $events = $this->creator->create('DynamicJSDomEvents');
         $eventsCollection = new ArrayCollection();
         foreach($eventsArray as $key => $value) {
-            $eventsCollection[$key] = $this->dynamicJDomEventManager->getCreator()->create('DynamicJSDomEvent');
-            $eventsCollection[$key]->setEventName($value);
-            $eventsCollection[$key]->setParentDynamicJDomEvents($events);
+            $eventsCollection[$key] = $this->creator->create('DynamicJSDomEvent');
+            self::configEvent($eventsCollection[$key], $value, $events);
         }
-        $events->setEvents($eventsCollection);
+        self::typedSetEvents($events, $eventsCollection);
         return $events;
+    }
+
+    private static function configEvent(iDynamicJSDomEvent &$event, $eventName, $parentEvents)
+    {
+        $event->setEventName($eventName);
+        $event->setParentDynamicJDomEvents($parentEvents);
+    }
+
+    private static function typedSetEvents(iDynamicJSDomEvents &$events, ArrayCollection $eventsCollection)
+    {
+        $events->setEvents($eventsCollection);
     }
 }
