9ddc8cf062df1e0a8d7e4e4bfd6a17fbec928cf4
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-01 13:46:42 -0500

    Added the iTrackerManager

---
+++ Services/iServices/iTrackerManager.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 11/1/13
+ * Time: 1:46 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services\iServices;
+
+use Cympel\Bundle\AnalyticsBundle\Entity\iType;
+
+interface iTrackerManager extends iType
+{
+
+}
