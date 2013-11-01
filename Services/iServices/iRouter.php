92863e215d0db06e78392dab649f70db95abde6a
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-01 14:28:53 -0500

    Added the generate method to iRouter

--- Services/iServices/iRouter.php
+++ Services/iServices/iRouter.php
@@ -9,8 +9,15 @@
 namespace Cympel\Bundle\AnalyticsBundle\Services\iServices;
 
 use Cympel\Bundle\AnalyticsBundle\Entity\iType;
+use Symfony\Component\Routing\Router;
 
 interface iRouter extends iType
 {
-
+    /**
+     * @param string $name
+     * @param mixed $parameters
+     * @param bool|string $referenceType
+     * @return string
+     */
+    public function generate($name, $parameters = array(), $referenceType = Router::ABSOLUTE_PATH);
 }
