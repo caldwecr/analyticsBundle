cf1901e2d263e7ff3c5072fd3acc7d434b223ced
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-01 15:59:57 -0500

    Added the iRoutedTrackingToolManagerExtender

---
+++ Services/iServices/iRoutedTrackingToolManagerExtensionService.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 11/1/13
+ * Time: 3:57 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services\iServices;
+
+interface iRoutedTrackingToolManagerExtender extends iTrackingToolManagerExtensionService
+{
+
+}
