6cfc23f694b9d034054fc0ff75ebe3e18edca086
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-01 16:03:46 -0500

    Added this exception type

---
+++ Services/Exception/InvalidExtensionServiceException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 11/1/13
+ * Time: 4:03 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services\Exception;
+
+class InvalidExtensionServiceException extends \Exception
+{
+
+}
