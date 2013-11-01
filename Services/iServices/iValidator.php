58bba75f3d72d9a77034d8f1867370857c7d93bd
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-01 10:24:24 -0500

    Added the iValidator interface

---
+++ Services/iServices/iValidator.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 11/1/13
+ * Time: 10:21 AM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services\iServices;
+
+use Cympel\Bundle\AnalyticsBundle\Entity\iType;
+use Cympel\Bundle\AnalyticsBundle\Entity\iEntity\iValidatable;
+
+interface iValidator extends iType
+{
+    public function validate(iValidatable $validatable);
+}
