06b40d4b01a686520f2d5b4f56caeba2364c511a
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-01 14:14:56 -0500

    Added the iRouter interface

---
+++ Services/iServices/iRouter.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 11/1/13
+ * Time: 2:11 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services\iServices;
+
+use Cympel\Bundle\AnalyticsBundle\Entity\iType;
+
+interface iRouter extends iType
+{
+    //Currently nothing implements this interface, it is included for completeness and to reserve the interface name for future use
+}
