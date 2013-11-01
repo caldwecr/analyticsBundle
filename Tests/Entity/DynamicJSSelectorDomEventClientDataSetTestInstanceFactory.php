78e39a538d0a3de8161d178a6098b14d9c772cc7
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-01 10:20:11 -0500

    Updated factory to handle null argument situation

--- Tests/Entity/DynamicJSSelectorDomEventClientDataSetTestInstanceFactory.php
+++ Tests/Entity/DynamicJSSelectorDomEventClientDataSetTestInstanceFactory.php
@@ -9,13 +9,19 @@
 namespace Cympel\Bundle\AnalyticsBundle\Tests\Entity;
 
 use Cympel\Bundle\AnalyticsBundle\Entity\DynamicJSSelectorDomEventClientDataSet;
+use Cympel\Bundle\AnalyticsBundle\Entity\DynamicJSSelectorDomEventClientDataSets;
 use Cympel\Bundle\AnalyticsBundle\Entity\iEntity\iDynamicJSSelectorDomEventClientDataSets;
+use Doctrine\Common\Collections\ArrayCollection;
 
 class DynamicJSSelectorDomEventClientDataSetTestInstanceFactory
 {
     public static function generate(iDynamicJSSelectorDomEventClientDataSets $parentDataSets = null)
     {
         $i = new DynamicJSSelectorDomEventClientDataSet();
+        if($parentDataSets == null) {
+            $parentDataSets = new DynamicJSSelectorDomEventClientDataSets();
+            $parentDataSets->setDataSets(new ArrayCollection(array($i)));
+        }
         $i->setParentDataSets($parentDataSets);
         $i->setRepositoryName('CympelAnalyticsBundle:DynamicJSSelectorDomEventClientDataSet');
         $i->setEntityManagerName('cympelanalytics');
