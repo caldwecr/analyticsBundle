a8cf0e8cf092738f2d93615ab2a79e1a13584b5c
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-01 10:51:40 -0500

    Added container aware unit test case of the validator service

---
+++ Tests/Services/CympelValidatorValidateTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 11/1/13
+ * Time: 10:44 AM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Tests\Services;
+
+use Cympel\Bundle\AnalyticsBundle\Entity\ConcretePersistableTestType;
+use Cympel\Bundle\AnalyticsBundle\Tests\ContainerAwareUnitTestCase;
+
+class CympelValidatorValidateTest extends ContainerAwareUnitTestCase
+{
+    public function testValidate()
+    {
+        $validator = $this->get('cympel_analytics.generics.validator');
+        $validatable = new ConcretePersistableTestType();
+        // Length of value needs to be between 2 and 10 so this should pass
+        $validatable->setValue('foo');
+        $this->assertEquals(0, count($validator->validate($validatable)));
+        $this->assertTrue($validator->isValid($validatable));
+        // Now try too short of a value
+        $validatable->setValue('a');
+        $this->assertEquals(1, count($validator->validate($validatable)));
+        $this->assertFalse($validator->isValid($validatable));
+        // Now try a value that is too long
+        $validatable->setValue('blahblahblahblah');
+        $this->assertEquals(1, count($validator->validate($validatable)));
+        $this->assertFalse($validator->isValid($validatable));
+    }
+}
