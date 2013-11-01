42cb3f79168cae46268a51e386b632fc3b921a3a
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-01 12:48:29 -0500

    Added the InvalidPersistableException

---
+++ Services/Exception/InvalidPersistableException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 11/1/13
+ * Time: 12:47 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services\Exception;
+
+class InvalidPersistableException extends \Exception
+{
+
+}
