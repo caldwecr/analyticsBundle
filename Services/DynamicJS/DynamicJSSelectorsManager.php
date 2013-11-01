d6804a369d925a9524228698274d968d1a964dd4
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-01 12:34:37 -0500

    Updates to DynamicJSSelectorsManager to clean it up, extend CympelManager, eliminate duplicate code, and force typing when logical
    Services configuration was updated for DynamicJSSelectorsManager to use the CympelManager constructor signature

--- Services/DynamicJS/DynamicJSSelectorsManager.php
+++ Services/DynamicJS/DynamicJSSelectorsManager.php
@@ -8,59 +8,15 @@
  */
 namespace Cympel\Bundle\AnalyticsBundle\Services\DynamicJS;
 
-use Cympel\Bundle\AnalyticsBundle\Services\CympelService;
-use Cympel\Bundle\AnalyticsBundle\Services\iServices\iCreator;
-use Cympel\Bundle\AnalyticsBundle\Services\iServices\iDynamicJSSelectorManager;
+use Cympel\Bundle\AnalyticsBundle\Entity\iEntity\iDynamicJSSelector;
+use Cympel\Bundle\AnalyticsBundle\Entity\iEntity\iDynamicJSSelectors;
+use Cympel\Bundle\AnalyticsBundle\Services\CympelManager;
 use Cympel\Bundle\AnalyticsBundle\Services\iServices\iDynamicJSSelectorsManager;
-use Cympel\Bundle\AnalyticsBundle\Services\iServices\iFinder;
-use Cympel\Bundle\AnalyticsBundle\Services\iServices\iPersister;
-use Cympel\Bundle\AnalyticsBundle\Services\iServices\iRemover;
 use Cympel\Bundle\AnalyticsBundle\Entity\DynamicJSSelectors;
 use Doctrine\Common\Collections\ArrayCollection;
 
-class DynamicJSSelectorsManager extends CympelService implements iDynamicJSSelectorsManager
+class DynamicJSSelectorsManager extends CympelManager implements iDynamicJSSelectorsManager
 {
-    /**
-     * @var iCreator
-     */
-    protected $creator;
-
-    /**
-     * @var iFinder
-     */
-    protected $finder;
-
-    /**
-     * @var iPersister
-     */
-    protected $persister;
-
-    /**
-     * @var iRemover
-     */
-    protected $remover;
-
-    /**
-     * @var iDynamicJSSelectorManager
-     */
-    protected $selectorManager;
-
-    /**
-     * @param iCreator $creator
-     * @param iFinder $finder
-     * @param iPersister $persister
-     * @param iRemover $remover
-     * @param iDynamicJSSelectorManager $selectorManager
-     */
-    public function __construct(iCreator $creator, iFinder $finder, iPersister $persister, iRemover $remover, iDynamicJSSelectorManager $selectorManager)
-    {
-        $this->creator = $creator;
-        $this->finder = $finder;
-        $this->persister = $persister;
-        $this->remover = $remover;
-        $this->selectorManager = $selectorManager;
-    }
-
     /**
      * @param $selectorArray
      * @return DynamicJSSelectors
@@ -69,47 +25,29 @@ class DynamicJSSelectorsManager extends CympelService implements iDynamicJSSelec
     {
         $selectors = $this->creator->create('DynamicJSSelectors');
         $selectorsCollection = new ArrayCollection();
+        $called = 0;
         foreach($selectorArray as $key => $value) {
-            $selectorsCollection[$key] = $this->selectorManager->getCreator()->create('DynamicJSSelector');
-            $selectorsCollection[$key]->setSelection($value);
-            $selectorsCollection[$key]->setParentSelectors($selectors);
-            $selectorsCollection[$key]->setCreated(time());
-            $selectorsCollection[$key]->setCalled(0);
+            $selectorsCollection[$key] = $this->creator->create('DynamicJSSelector');
+            $created = time();
+            self::configureSelector($selectorsCollection[$key], $value, $selectors, $created, $called);
         }
         $selectors->setSelectors($selectorsCollection);
         return $selectors;
     }
 
     /**
-     * @return iCreator
+     * @param iDynamicJSSelector $selector
+     * @param string $selection
+     * @param iDynamicJSSelectors $parentSelectors
+     * @param int $created
+     * @param int $called
      */
-    public function getCreator()
+    private static function configureSelector(iDynamicJSSelector $selector, $selection, iDynamicJSSelectors $parentSelectors, $created, $called)
     {
-        return $this->creator;
-    }
-
-    /**
-     * @return iFinder
-     */
-    public function getFinder()
-    {
-        return $this->finder;
-    }
-
-    /**
-     * @return iPersister
-     */
-    public function getPersister()
-    {
-        return $this->persister;
-    }
-
-    /**
-     * @return iRemover
-     */
-    public function getRemover()
-    {
-        return $this->remover;
+        $selector->setSelection($selection);
+        $selector->setParentSelectors($parentSelectors);
+        $selector->setCreated($created);
+        $selector->setCalled($called);
     }
 
     /**
