cb62f3b5f8a38996d79700e791779fcfa0950772
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-04 10:51:47 -0600

    Added the NotNull assertion

--- Entity/DynamicJSSelector.php
+++ Entity/DynamicJSSelector.php
@@ -33,6 +33,7 @@ class DynamicJSSelector extends CympelType implements iDynamicJSSelector
      * @var string
      * @ORM\Column(type="string", length=255)
      * @Assert\Length(min=2, max=255)
+     * @Assert\NotNull()
      */
     protected $selection;
 
