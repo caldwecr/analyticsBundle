71cb1ada963e2c2502270a382e345d52017127fb
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-04 14:14:43 -0600

    Added the ClientDataSetDomClassCreate test

---
+++ Tests/Services/DynamicJS/ClientDataSetDomClassCreateTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 11/4/13
+ * Time: 2:12 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Tests\Services\DynamicJS;
+
+use Cympel\Bundle\AnalyticsBundle\Tests\ContainerAwareUnitTestCase;
+
+class ClientDataSetDomClassCreateTest extends ContainerAwareUnitTestCase
+{
+    public function testCreate()
+    {
+        $creator = $this->get('cympel_analytics.generics.creator');
+        $clientDataSetDomClass = $creator->create('DynamicJSSelectorDomEventClientDataSetDomClass');
+        $this->assertEquals('DynamicJSSelectorDomEventClientDataSetDomClass', $clientDataSetDomClass->getType());
+    }
+}
