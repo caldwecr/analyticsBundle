463d27fe7d46643d7190883bef86cf878037db28
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-04 11:25:57 -0600

    Added the UnpersistedFindByException

---
+++ Services/Exception/UnpersistedFindByException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 11/4/13
+ * Time: 11:25 AM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services\Exception;
+
+class UnpersistedFindByException extends \Exception
+{
+
+}
