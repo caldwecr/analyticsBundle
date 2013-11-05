fe447e6acfb1998b2f7f670806ae1a115e5b9668
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-05 15:57:43 -0600

    Added the DuplicateCympelNamespaceException

---
+++ Entity/Exception/DuplicateCympelNamespaceException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 11/5/13
+ * Time: 3:57 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity\Exception;
+
+class DuplicateCympelNamespaceException extends \Exception
+{
+
+}
