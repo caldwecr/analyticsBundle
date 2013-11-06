f5a4631173bbba7d1591f0918cbd9e535a489961
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-05 19:56:25 -0600

    Added another exception type

---
+++ Entity/Exception/InvalidAttemptToRemoveEntityException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 11/5/13
+ * Time: 7:56 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity\Exception;
+
+class InvalidAttemptToRemoveEntityException extends \Exception
+{
+
+}
