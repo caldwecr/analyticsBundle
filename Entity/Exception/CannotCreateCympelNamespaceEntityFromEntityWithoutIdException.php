0fe38ab690c735c3688cf7dc4d33d85033757d5b
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-06 09:49:22 -0600

    Added another exception

---
+++ Entity/Exception/CannotCreateCympelNamespaceEntityFromEntityWithoutIdException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 11/6/13
+ * Time: 9:49 AM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity\Exception;
+
+class CannotCreateCympelNamespaceEntityFromEntityWithoutIdException extends \Exception
+{
+
+}
