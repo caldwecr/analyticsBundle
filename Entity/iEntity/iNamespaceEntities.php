1b48f2d74a7eb7c94136c80b6bf8f06d12536ad3
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-06 10:05:18 -0600

    Added two methods to the interface

---
+++ Entity/iEntity/iNamespaceEntities.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 11/5/13
+ * Time: 8:20 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity\iEntity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+interface iNamespaceEntities extends iType
+{
+    /**
+     * @return void
+     *
+     * Sets the index of the iNamespaceableEntities entity to stale
+     */
+    public function makeIndexStale();
+
+    /**
+     * @return void
+     */
+    public function refreshIndex();
+
+    /**
+     * @param $entitiesArrayCollection
+     * @return void
+     */
+    public function setEntitiesArrayCollection(ArrayCollection $entitiesArrayCollection);
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getEntitiesArrayCollection();
+}
