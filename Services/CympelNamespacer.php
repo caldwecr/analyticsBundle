57bc8dfc15dfa51728c57147830ae68d15c0b71b
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-05 20:15:51 -0600

    Created the CympelNamespacer service

---
+++ Services/CympelNamespacer.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 11/5/13
+ * Time: 7:02 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Services;
+
+use Cympel\Bundle\AnalyticsBundle\Entity\CympelNamespace;
+use Cympel\Bundle\AnalyticsBundle\Entity\Exception\InvalidAttemptToRemoveCympelNamespaceException;
+use Cympel\Bundle\AnalyticsBundle\Entity\iEntity\iNamespace;
+use Cympel\Bundle\AnalyticsBundle\Entity\iEntity\iNamespaceable;
+use Cympel\Bundle\AnalyticsBundle\Services\iServices\iNamespacer;
+use Cympel\Bundle\AnalyticsBundle\Entity\Exception\InvalidAttemptToSetEntityCympelNamespace;
+
+class CympelNamespacer extends CympelService implements iNamespacer
+{
+    /**
+     * @param iNamespaceable $entity
+     * @param iNamespace $cympelNamespace
+     * @return int
+     * @throws \Cympel\Bundle\AnalyticsBundle\Entity\Exception\InvalidAttemptToSetEntityCympelNamespace
+     */
+    public function addEntityToCympelNamespace(iNamespaceable $entity, iNamespace $cympelNamespace)
+    {
+        $ens = $entity->getCympelNamespace();
+        if(!$ens || $ens->equals(CympelNamespace::getBlankCympelNamespace())) {
+            $entity->setCympelNamespace($cympelNamespace);
+            $cympelNamespace->append($entity);
+        } else {
+            throw new InvalidAttemptToSetEntityCympelNamespace();
+        }
+        return $cympelNamespace->getEntityCount();
+
+    }
+
+    /**
+     * @param iNamespaceable $entity
+     * @param iNamespace $cympelNamespace
+     * @return int
+     * @throws \Cympel\Bundle\AnalyticsBundle\Entity\Exception\InvalidAttemptToRemoveCympelNamespaceException
+     */
+    public function removeEntityFromCympelNamespace(iNamespaceable $entity, iNamespace $cympelNamespace)
+    {
+        if($entity->getCympelNamespace()->equals($cympelNamespace)) {
+            $entity->setCympelNamespace(CympelNamespace::getBlankCympelNamespace());
+            $cympelNamespace->remove($entity);
+        } else {
+            throw new InvalidAttemptToRemoveCympelNamespaceException();
+        }
+        return $cympelNamespace->getEntityCount();
+    }
+
+    /**
+     * @return string
+     * This method must return a string with a unique representation of the object type that is implementing this interface
+     */
+    public function getType()
+    {
+        return 'CympelNamespacer';
+    }
+
+}
