06e8da4be88e4bb77fc54a75aee4f76f2ffd76c2
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-05 20:49:39 -0600

    Add another exception type

---
+++ Entity/Exception/EntityNotFoundInCympelNamespaceException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 11/5/13
+ * Time: 8:49 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity\Exception;
+
+class EntityNotFoundInCympelNamespaceException extends \Exception
+{
+
+}
