05a254b7469a690cc1009dc085d101e3da8de648
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-11 23:04:07 -0600

    Added the iResourceServerController interface

---
+++ Controller/iController/iResourceServerController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 11/11/13
+ * Time: 11:02 PM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Controller\iController;
+
+interface iResourceServerController
+{
+}
