a09ab61c7cd221301fab659226970a00b6e1b79d
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-20 11:03:54 -0600

    Added the InvalidReportRunStatusException

---
+++ Entity/Exception/InvalidReportRunStatusException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 11/20/13
+ * Time: 11:03 AM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity\Exception;
+
+class InvalidReportRunStatusException extends \Exception
+{
+
+}
