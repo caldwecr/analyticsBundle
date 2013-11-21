18a898ef4314e2b07b2d121f19da6a5e8ab85fad
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-21 16:48:47 -0600

    Fix typo
    Signed-off-by: Courtland Caldwell <courtland.caldwell@cympel.com>

--- Services/CympelNamespacer.php
+++ Services/CympelNamespacer.php
@@ -97,7 +97,7 @@ class CympelNamespacer extends CympelService implements iNamespacer
             if(!$ens) {
                 throw new InvalidAttemptToSetEntityCympelNamespace("Namespace was null");
             } else {
-                throw new InvalidAttemptToSetEntityCympelNamespace('This entity already has a namespace associated with it, to change the namespace first remove the existing one.' . $ens->getName(), $cympelNamespace->getName());
+                throw new InvalidAttemptToSetEntityCympelNamespace('This entity already has a namespace associated with it, to change the namespace first remove the existing one. ENS = ' . $ens->getName() . ' cn=' . $cympelNamespace->getName());
             }
         }
         return $cympelNamespace->getEntityCount();
