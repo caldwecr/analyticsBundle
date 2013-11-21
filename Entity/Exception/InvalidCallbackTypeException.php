7be7800668e300fd50c37d8b6bc1f474abd49243
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-21 10:41:03 -0600

    Added new exception type

---
+++ Entity/Exception/InvalidCallbackTypeException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caldwecr
+ * Date: 11/21/13
+ * Time: 10:40 AM
+ * Copyright Cympel Inc
+ */
+namespace Cympel\Bundle\AnalyticsBundle\Entity\Exception;
+
+class InvalidCallbackTypeException extends \Exception
+{
+
+}
