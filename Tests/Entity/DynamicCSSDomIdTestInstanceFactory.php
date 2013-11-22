cbb7ad35bd23bb46cc23ec2be3b249e3502ca7d9
Courtland Caldwell <courtland.caldwell@cympel.com>
2013-11-22 14:44:48 -0600

    Added functionality to the test instance factory for DynamicCSSDomId entities to support being created using an iCreator

--- Tests/Entity/DynamicCSSDomIdTestInstanceFactory.php
+++ Tests/Entity/DynamicCSSDomIdTestInstanceFactory.php
@@ -11,13 +11,19 @@ namespace Cympel\Bundle\AnalyticsBundle\Tests\Entity;
 use Cympel\Bundle\AnalyticsBundle\Entity\DynamicCSS;
 use Cympel\Bundle\AnalyticsBundle\Entity\DynamicCSSDomId;
 use Cympel\Bundle\AnalyticsBundle\Entity\DynamicCSSDomIdArrayCollection;
+use Cympel\Bundle\AnalyticsBundle\Services\iServices\iCreator;
 
 class DynamicCSSDomIdTestInstanceFactory
 {
-    public static function createInstance($domIdValue = 'validValue')
+    public static function createInstance($domIdValue = 'validValue', iCreator $creator = null)
     {
-        $dcss = new DynamicCSS();
-        $dcdi = new DynamicCSSDomId();
+        if($creator) {
+            $dcss = $creator->create('DynamicCSS');
+            $dcdi = $creator->create('DynamicCSSDomId');
+        } else {
+            $dcss = new DynamicCSS();
+            $dcdi = new DynamicCSSDomId();
+        }
         $ids = new DynamicCSSDomIdArrayCollection(array($dcdi));
         $dcss->setDynamicCSSDomIds($ids);
         $dcdi->setRendered(0);
